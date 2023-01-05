ca0f35586bdc54f6d859b67754b240a941358bcc
Thomaslj <dumle975@hotmail.com>
2023-01-04 22:35:25 -0800

    resolve merge conflict

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -5,7 +5,6 @@ import pandas as pd
 import pickle
 
 from sklearn.model_selection import train_test_split, GridSearchCV
-<<<<<<< HEAD
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.multioutput import MultiOutputClassifier
@@ -17,21 +16,12 @@ import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
-=======
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix
-
-from sqlalchemy import create_engine
->>>>>>> 3461bcf41f583ceeecc3d96a9c7a00c64f2e8a35
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 
 
 def load_data(database_filepath):
-<<<<<<< HEAD
     """
     loads the data saved in a db. 
     Input:
@@ -64,36 +54,16 @@ def tokenize(text):
     lemmatizer = WordNetLemmatizer()
     text = text.lower()
     text = re.sub(r'[^A-Za-z0-9 ]+', '', text)
-=======
-    db_path = 'sqlite:///' + os.getcwd() + '/' + database_filepath
-    engine = create_engine(db_path)
-    df = pd.read_sql('SELECT * FROM disaster_table', engine)
-    df = df.drop(columns=['id', 'original', 'genre'])
-    X = df['message']
-    Y = df.loc[:, df.columns != 'message']
-    return X, Y, Y.columns
-
-def tokenize(text):
-    stop_words = stopwords.words("english")
-    lemmatizer = WordNetLemmatizer()
-    text = text.lower()
->>>>>>> 3461bcf41f583ceeecc3d96a9c7a00c64f2e8a35
     url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     detected_urls = re.findall(url_regex, text)
     for url in detected_urls:
         text = text.replace(url, "urlplaceholder")
-<<<<<<< HEAD
     tokenized = word_tokenize(text)
     tokens = [lemmatizer.lemmatize(word) for word in tokenized if word not in stop_words]
-=======
-    tokens = word_tokenize(text)
-    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
->>>>>>> 3461bcf41f583ceeecc3d96a9c7a00c64f2e8a35
     return tokens
 
 
 def build_model():
-<<<<<<< HEAD
     """
     This functions builds the data processing pipeline.
     ------------
@@ -135,32 +105,6 @@ def save_model(model, model_filepath):
     """
     Save the trained model in model_filepath (str).
     """
-=======
-    pipeline = Pipeline([
-        ('vect', CountVectorizer(tokenizer=tokenize)),
-        ('tfidf', TfidfTransformer()),
-        ('clf', RandomForestClassifier())
-    ])
-    parameters = {
-        'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
-        'clf__n_estimators': [50, 100, 200],
-        'clf__min_samples_split': [2, 3, 4]
-    }
-
-    cv = GridSearchCV(pipeline, param_grid=parameters)
-
-
-def evaluate_model(model, X_test, y_test, category_names):
-    y_pred = model.predict(X_test)
-    confusion_mat = confusion_matrix(y_test, y_pred, labels=category_names)
-    accuracy = (y_pred == y_test).mean()
-
-    print("Labels:", category_names)
-    print("Confusion Matrix:\n", confusion_mat)
-    print("Accuracy:", accuracy)
-
-def save_model(model, model_filepath):
->>>>>>> 3461bcf41f583ceeecc3d96a9c7a00c64f2e8a35
     pickle.dump(model, open(model_filepath, 'wb'))
 
 
@@ -170,10 +114,6 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-<<<<<<< HEAD
-=======
-        tokenize(X.iloc[0].message)
->>>>>>> 3461bcf41f583ceeecc3d96a9c7a00c64f2e8a35
         print('Building model...')
         model = build_model()
         print('Training model...')
